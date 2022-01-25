4f2c66249d1db68e87b9c6c73c4fbc95eb1523ac
MainBot <65481692+rssdevoloper@users.noreply.github.com>
2022-01-24 22:42:02 -0800

    Add files via upload

---
+++ plugins/menu.js
@@ -0,0 +1,215 @@
+let levelling = require('../lib/levelling')
+let { MessageType } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let path = require('path')
+let fetch = require('node-fetch')
+let moment = require('moment-timezone')
+let tags = {
+    'game': '🔖 _*G A M E*_ 🔖',
+    'xp': '🔖 _*E X P & L I M I T*_ 🔖',
+    'sticker': '🔖 _*S T I C K E R*_ 🔖',
+    'kerang': '🔖 _*K E R A N G*_  🔖',
+    'random': '🔖 _*R A N D O M  A N I M E*_ 🔖',
+    'quotes': '🔖 _*Q U O T E S*_ 🔖',
+    'admin': '🔖 _*A D M I N*_ 🔖',
+    'group': '🔖 _*G R O U P*_ 🔖',
+    'internet': '🔖 _*I N T E R N E T*_ 🔖',
+    'anonymous': '🔖 _*A N O N C H A T*_ 🔖',
+    'nulis': '🔖 _*M A K E R*_ 🔖',
+    'downloader': '🔖 _*D O W N L O A D E R*_ 🔖',
+    'tools': '🔖 _*T O O L S*_ 🔖',
+    'fun': '🔖 _*F U N*_ 🔖',
+    'database': '🔖 _*D A T A B A S E*_ 🔖',
+    'vote': '🔖 _*V O T I N G*_ 🔖',
+    'absen': '🔖 _*A B S E N*_ 🔖',
+    'quran': '🔖 _*I S L A M I*_ 🔖',
+    'audio': '🔖 _*S O U N D*_ 🔖',
+    'info': '🔖 _*I N F O R M A T I O N*_ 🔖',
+  }
+
+const defaultMenu = {
+  before: `
+*HAI %name!* 👋 *Welcome have a nice day* ✨
+
+  🔖 _*U S E R S*_ 🔖
+
+🔖 _NAME: %name_
+🔖 _LIMIT: %limit_
+🔖 _ROLE: %role_
+🔖 _LEVEL: %level / %maxexp_
+🔖 _XP:  %exp / %totalexp_
+
+
+   🔖 _*E R Z A  I N F O*_ 🔖
+
+🔖 _DATE: %week %date_
+🔖 _ISLAMI: %dateIslamic_
+🔖 _TIME: %time_
+🔖 _RUNTIME: %uptime_
+🔖 _USERS: %rtotalreg_
+🔖 _WEB: https://erzaa.site_
+🔖 _Api: https://api.sekha.tech_
+🔖 _Owner: https://github.com/inirey_
+
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+%readmore`.trimStart(),
+  header: '%category\n\n',
+  body: '🔖 _%cmd_',
+  footer: '\n',
+  after: `
+*%npmname*
+${'```%npmdesc```'}
+`,
+}
+let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
+  try {
+    let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
+    let { exp, limit, level, role, registered } = global.db.data.users[m.sender]
+    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let name = registered ? global.db.data.users[m.sender].name : conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'id'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+      return {
+        help: Array.isArray(plugin.help) ? plugin.help : [plugin.help],
+        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+        prefix: 'customPrefix' in plugin,
+        limit: plugin.limit,
+        premium: plugin.premium,
+        enabled: !plugin.disabled,
+      }
+     })
+    let groups = {}
+    for (let tag in tags) {
+      groups[tag] = []
+      for (let plugin of help)
+        if (plugin.tags && plugin.tags.includes(tag))
+          if (plugin.help) groups[tag].push(plugin)
+      // for (let tag of plugin.tags)
+      //   if (!(tag in tags)) tags[tag] = tag
+    }
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Dipersembahkan oleh https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let _text = [
+      before,
+      ...Object.keys(tags).map(tag => {
+        return header.replace(/%category/g, tags[tag]) + '\n' + [
+          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+            return menu.help.map(help => {
+              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+                .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
+                .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
+                .trim()
+            }).join('\n')
+          }),
+          footer
+        ].join('\n')
+      }),
+      after
+    ].join('\n')
+    text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    let replace = {
+      '%': '%',
+      p: _p, uptime, muptime,
+      me: conn.user.name,
+      npmname: package.name,
+      npmdesc: package.description,
+      version: package.version,
+      exp: exp - min,
+      maxexp: xp,
+      totalexp: exp,
+      xp4levelup: max - exp <= 0 ? `Siap untuk *${_p}levelup*` : `${max - exp} XP lagi untuk levelup`,
+      github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
+      level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+      readmore: readMore
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    await conn.send3ButtonLoc(m.chat, await (await fetch(reyganz + 'APIKEY')).buffer(), text.trim(), '© by sekha', 'Owner Bot', `,owner`, 'Group Wa', `,wanodejs`, 'Donasi owner', `,donasi`, m)
+  } catch (e) {
+    conn.reply(m.chat, 'Maaf, menu sedang error', m)
+    throw e
+  }
+}
+handler.help = ['menu', 'help', '?']
+handler.tags = ['main']
+handler.command = /^(menu|help|\?)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 3
+
+module.exports = handler
+
+const more = String.fromCharCode(1)
+const readMore = more.repeat(1)
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+function ucapan() {
+  const time = moment.tz('Asia/Jakarta').format('HH')
+  res = "Selamat dinihari"
+  if (time >= 4) {
+    res = "Selamat pagi"
+  }
+  if (time > 10) {
+    res = "Selamat siang"
+  }
+  if (time >= 15) {
+    res = "Selamat sore"
+  }
+  if (time >= 18) {
+    res = "Selamat malam"
+  }
+  return res
+}
